35721220b85c8118bf4703a0d7966e81dcfd3651
Leaking <chenhuazhaoao@gmail.com>
2015-03-07 13:42:38 +0800

    implement it by View instead of Framelayout

--- Example/TestLibs/src/com/example/testlibs/MainActivity.java
+++ Example/TestLibs/src/com/example/testlibs/MainActivity.java
@@ -18,27 +18,10 @@ public class MainActivity extends Activity implements SlideListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SlideSwitch slide = (SlideSwitch) findViewById(R.id.swit);
-        slide.setState(false);
+        //slide.setState(false);
         txt = (TextView) findViewById(R.id.txt);
         slide.setSlideListener(this);
     }
-
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    
 
 
 
--- SlideSwitch/src/com/leaking/slideswitch/SlideSwitch.java
+++ SlideSwitch/src/com/leaking/slideswitch/SlideSwitch.java
@@ -2,9 +2,16 @@ package com.leaking.slideswitch;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Looper;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.TranslateAnimation;
@@ -13,227 +20,227 @@ import android.widget.RelativeLayout;
 
 import com.example.slideswitch.R;
 
-public class SlideSwitch extends FrameLayout {
+public class SlideSwitch extends View {
+
+	private static final int RIM_SIZE = 6;
+
+	private static final int COLOR_THEME = Color.parseColor("#ff00ee00");
 	
-	public interface SlideListener{
+	//ÊôÐÔ
+	private int color_theme;
+	private boolean isOpen = false;
+	
+	
+	
+	
+	private Paint paint;
+	private Rect backRect;
+	private Rect frontRect;
+	private int frontRect_left;
+	private int max_left;
+	private int min_left;
+	private int frontRect_left_begin = RIM_SIZE;
+
+	
+	private int eventStartX;
+	private int eventLastX;
+	private int diffX = 0;
+	private int alpha;
+
+	public interface SlideListener {
 		public void open();
+
 		public void close();
 	}
-	
-	private static final int MOVETIME = 150;
 
-	private static final int LP_MATCH = LayoutParams.MATCH_PARENT;
-	private static final int LP_WRAPP = LayoutParams.WRAP_CONTENT;
-
-	
 	private SlideListener listener;
-
-
-	boolean isOpen = false;
-	int moveLength;
-	
-	int baseColor;
-	int stillColor;
-	int moveColor;
-
-	int rimSize;
-	int baseHeight;
-	int baseWidth;
-
-	RelativeLayout baseView;
-	RelativeLayout stillView;
-	RelativeLayout moveableView;
 
 	public SlideSwitch(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-		init();
+		TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.slideswitch);
+		color_theme = a.getColor(R.styleable.slideswitch_themeColor, COLOR_THEME);
+		isOpen = a.getBoolean(R.styleable.slideswitch_isOpen, false);
+		paint = new Paint();
+
 	}
 
 	public SlideSwitch(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		TypedArray a = context
-				.obtainStyledAttributes(attrs, R.styleable.slideswitch);
-		baseColor = a.getColor(R.styleable.slideswitch_baseColor, baseColor);
-		stillColor = a.getColor(R.styleable.slideswitch_stillColor, stillColor);
-		moveColor = a.getColor(R.styleable.slideswitch_moveColor, moveColor);
-		baseWidth = a.getDimensionPixelSize(R.styleable.slideswitch_slideWidth, baseWidth);
-		//baseWidth = px2dip(baseWidth);
-		baseHeight = a.getDimensionPixelSize(R.styleable.slideswitch_slideHeight, baseHeight);
-		//baseHeight = px2dip(baseHeight);
-		isOpen = a.getBoolean(R.styleable.slideswitch_isOpen, isOpen);
-		rimSize = 6;
-		init();
+		this(context, attrs, 0);
 	}
 
 	public SlideSwitch(Context context) {
-		super(context);
-		init();
+		this(context, null);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// TODO Auto-generated method stub
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		int width = measureDimension(80, widthMeasureSpec);
+		int height = measureDimension(30, heightMeasureSpec);
+		setMeasuredDimension(width, height);
+		backRect = new Rect(0, 0, width, height);
+		
+		
+		if(isOpen){
+			frontRect_left = width/2;
+			alpha = 255;
+		}else{
+			frontRect_left = RIM_SIZE;
+			alpha = 0;
+		}
+
+		frontRect_left_begin = frontRect_left;
+		min_left = RIM_SIZE;
+		max_left = width / 2;
+		
+
+
+	}
+
+
+
+	public int measureDimension(int defaultSize, int measureSpec) {
+		int result;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
+		if (specMode == MeasureSpec.EXACTLY) {
+			result = specSize;
+		} else {
+			result = defaultSize; // UNSPECIFIED
+			if (specMode == MeasureSpec.AT_MOST) {
+				result = Math.min(result, specSize);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		paint.setColor(Color.GRAY);
+		canvas.drawRect(backRect, paint);
+
+		paint.setColor(color_theme);
+		paint.setAlpha(alpha);
+		canvas.drawRect(backRect, paint);
+
+		frontRect = new Rect(frontRect_left, RIM_SIZE, frontRect_left
+				+ getMeasuredWidth() / 2 - RIM_SIZE, getMeasuredHeight() - 2
+				* RIM_SIZE);
+		paint.setColor(Color.WHITE);
+		canvas.drawRect(frontRect, paint);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		int action = MotionEventCompat.getActionMasked(event);
+
+		switch (action) {
+		case MotionEvent.ACTION_DOWN:
+			eventStartX = (int) event.getRawX();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			eventLastX = (int) event.getRawX();
+			diffX = eventLastX - eventStartX;
+			int tempX = diffX + frontRect_left_begin;
+			if (tempX > min_left && tempX < max_left) {
+				frontRect_left = tempX;
+				alpha = (int) (255 * (float) tempX / (float) max_left);
+				invalidateView();
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			int wholeX = (int) (event.getRawX() - eventStartX);
+			
+			
+			frontRect_left_begin = frontRect_left;
+			boolean toRight;
+			toRight = (frontRect_left_begin > max_left / 2 ? true : false);
+			System.out.println("wholeXXXX = " + wholeX);
+			if(Math.abs(wholeX) < 3 || wholeX == 0){
+				toRight = !toRight;
+				System.out.println("toRight =  " + toRight);
+			}
+			moveToDest(toRight);
+
+			break;
+		default:
+			break;
+		}
+		return true;
 	}
 
 	/**
-	 * 
+	 * ÖØ»æ
 	 */
-	public void init() {
-		this.setBackgroundColor(this.baseColor);
-		
-		baseView = new RelativeLayout(getContext());
-		stillView = new RelativeLayout(getContext());
-		moveableView = new RelativeLayout(getContext());
-		LayoutParams baseParams = new LayoutParams(LP_WRAPP,LP_WRAPP);
-		LayoutParams stillParams = new LayoutParams(LP_WRAPP, LP_WRAPP);
-		LayoutParams moveParams = new LayoutParams(LP_WRAPP, LP_WRAPP);
-		baseParams.gravity = Gravity.CENTER;
-		stillParams.gravity = Gravity.RIGHT;
-
-
-		
-		baseParams.width = baseWidth - rimSize*2;
-		baseParams.height = baseHeight - rimSize*2;
-		
-		stillParams.width = (baseWidth - 2*rimSize) / 2;
-		stillParams.height = baseHeight - rimSize*2;
-		moveParams.width = (baseWidth - 2*rimSize) / 2;
-		moveParams.height = baseHeight - rimSize*2;
-		
-		moveLength = stillParams.width;
-		
-		baseParams.bottomMargin = rimSize;
-		baseParams.topMargin = rimSize;
-		baseParams.leftMargin = rimSize;
-		baseParams.rightMargin = rimSize;
-		
-		
-		moveParams.bottomMargin = rimSize;
-		moveParams.topMargin = rimSize;
-		
-		
-		stillParams.rightMargin = rimSize;
-		stillParams.topMargin = rimSize;
-		stillParams.bottomMargin = rimSize;
-		
-		moveableView.setBackgroundColor(this.moveColor);
-		stillView.setBackgroundColor(this.stillColor);
-		
-		baseView.setLayoutParams(baseParams);
-		stillView.setLayoutParams(stillParams);
-		moveableView.setLayoutParams(moveParams);
-		
-		System.out.println("isOpen == " + isOpen);
-		
-		if(isOpen == false){
-			moveParams.gravity = Gravity.LEFT;
-			this.setBackgroundColor(this.stillColor);
-			moveParams.leftMargin = rimSize;
-		}else{
-			moveParams.gravity = Gravity.RIGHT;
-			this.setBackgroundColor(this.baseColor);
-			moveParams.rightMargin = rimSize;
+	private void invalidateView() {
+		if (Looper.getMainLooper() == Looper.myLooper()) {
+			invalidate();
+		} else {
+			postInvalidate();
 		}
-
-		
-		
-		addView(baseView, baseParams);
-		addView(stillView, stillParams);
-		addView(moveableView, moveParams);
-		
-
 	}
 
-	public int px2dip(float pxValue) { 
-        final float scale = this.getResources().getDisplayMetrics().density; 
-        return (int) (pxValue / scale + 0.5f); 
-    } 
-	
-	@Override
-	public boolean dispatchTouchEvent(MotionEvent ev) {
-		// TODO Auto-generated method stub
-		if(!isOpen){
-			TranslateAnimation tAnim = new TranslateAnimation(0,moveLength,0,0);
-			tAnim.setDuration(MOVETIME);
-			moveableView.startAnimation(tAnim);
-			tAnim.setAnimationListener(new AnimationListener() {
-				
-				@Override
-				public void onAnimationStart(Animation animation) {
-					// TODO Auto-generated method stub
-					SlideSwitch.this.setBackgroundColor(baseColor);
-				}
-				
-				@Override
-				public void onAnimationRepeat(Animation animation) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void onAnimationEnd(Animation animation) {
-					// TODO Auto-generated method stub
-					FrameLayout.LayoutParams flp = (LayoutParams) moveableView.getLayoutParams();
-					flp.gravity = Gravity.RIGHT;
-					flp.rightMargin = rimSize;
-					moveableView.setLayoutParams(flp);
-					moveableView.clearAnimation();
-					listener.open();
-					isOpen = !isOpen;
-				}
-			});
-		}else{
-			TranslateAnimation tAnim = new TranslateAnimation(0,-moveLength,0,0);
-			tAnim.setDuration(MOVETIME);
-			moveableView.startAnimation(tAnim);
-			tAnim.setAnimationListener(new AnimationListener() {
-				
-				@Override
-				public void onAnimationStart(Animation animation) {
-					// TODO Auto-generated method stub
-					
-
-				}
-				
-				@Override
-				public void onAnimationRepeat(Animation animation) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void onAnimationEnd(Animation animation) {
-					// TODO Auto-generated method stub
-					FrameLayout.LayoutParams flp = (LayoutParams) moveableView.getLayoutParams();
-					flp.gravity = Gravity.LEFT;
-					flp.leftMargin = rimSize;
-					moveableView.setLayoutParams(flp);
-					moveableView.clearAnimation();
-					isOpen = !isOpen;
-					listener.close();
-					SlideSwitch.this.setBackgroundColor(stillColor);
-				}
-			});
-			
-		}
-		return super.dispatchTouchEvent(ev);
-	}
-	
 	public void setSlideListener(SlideListener listener) {
 		this.listener = listener;
 	}
-	
-    public void setState(boolean isOpen){
-    	if(isOpen){
-    		FrameLayout.LayoutParams flp = (LayoutParams) moveableView.getLayoutParams();
-			flp.gravity = Gravity.RIGHT;
-			flp.rightMargin = rimSize;
-			moveableView.setLayoutParams(flp);
-			SlideSwitch.this.setBackgroundColor(baseColor);
-    	}else{
-    		FrameLayout.LayoutParams flp = (LayoutParams) moveableView.getLayoutParams();
-			flp.gravity = Gravity.LEFT;
-			flp.leftMargin = rimSize;
-			moveableView.setLayoutParams(flp);
-			
-			SlideSwitch.this.setBackgroundColor(stillColor);
-    	}
-		this.isOpen = isOpen;
 
-    }
+	public void moveToDest(final boolean toRight) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("-----111");
+				if(toRight){
+					while(frontRect_left <= max_left){
+						alpha = (int) (255 * (float) frontRect_left / (float) max_left);
+						invalidateView();
+						frontRect_left += 3;
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					frontRect_left_begin = max_left;
+				}else{
+					while(frontRect_left >= min_left){
+						alpha = (int) (255 * (float) frontRect_left / (float) max_left);
+						invalidateView();
+						frontRect_left -= 3;
+						try {
+							Thread.sleep(3);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+					frontRect_left_begin = min_left;
+				}
+			}
+		}).start();
+
+	}
+
+	private static class AnimationListenerAdapter implements AnimationListener {
+
+		@Override
+		public void onAnimationStart(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
 }
